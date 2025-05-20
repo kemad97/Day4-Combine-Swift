62dceec6077aef8cc5169f9fd4819f1fc0662892
kemad97 <kerolos.emad992@gmail.com>
2025-05-20 14:01:49 +0300

    d

--- Day4 Combine Swift/ViewController.swift
+++ Day4 Combine Swift/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Combine
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct News:Decodable {
+    let title : String
+    let author:String
 }
 
+
+class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = URL(string: "https://raw.githubusercontent.com/DevTides/NewsApi/master/news.json")!
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [News].self, decoder: JSONDecoder())
+           // .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("Done")
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+                
+            }, receiveValue: { newsList in
+                guard let first = newsList.first else { return }
+                self.titleLabel.text = first.title
+                self.authorLabel.text = first.author
+            })
+            .store(in: &cancellables)
+    }
+}
