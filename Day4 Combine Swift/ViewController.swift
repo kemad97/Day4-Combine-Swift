0e8d7d20d79f785fc16d742efe3ab5152b4b2022
kemad97 <kerolos.emad992@gmail.com>
2025-05-21 13:42:22 +0300

    d

--- Day4 Combine Swift/ViewController.swift
+++ Day4 Combine Swift/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var publishLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    private var cancellables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        publishLabel.text = "..."
+        counterLabel.text = "..."
+        
+        Just ("Hello MAD 45")
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .sink{
+                [weak self] value in
+                self?.publishLabel.text = value
+            }
+            .store(in: &cancellables)
+        
+        
+        var count = 0
+        
+        Timer.publish(every: 1.0, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                count += 1
+                if count <= 5 {
+                    self?.counterLabel.text = "\(count)"
+                }
+                
+            }.store(in: &cancellables)
+        
     }
-
-
+    
+    
 }
 
